901ef02835dbd5dcb3aeefd1cf6c6e8df179458f
Kneeslapper <gloriaklee8@gmail.com>
2020-01-10 23:00:56 -0800

    added article.js requirements headline, summary, URL

--- models/Article.js
+++ models/Article.js
@@ -1,30 +1,30 @@
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
 //save a reference to the schema constructor
-var Schema = mongoose.Schema;
+let Schema = mongoose.Schema;
 
 //using schema contructor, create a new UserSchema object
 var ArticleSchema = new Schema({
-    //'title is required and of type String
-    title: {
+    //'headline' required and of type string
+    headline: {
         type: String,
         required: true
     },
     // 'link' is required and of type string
-    link: {
+    summary: {
         type: String,
         required: true
     },
     //not is an object that stores a Note ID
     //ref property links the ObjectId to the Note model
     //this allows us to populate the Article with an associated Note
-    note: {
-        type: Schema.Types.ObjectId,
-        ref: "Note"
+    URL: {
+        type: String,
+        required: true
     }
 });
 //this creates our model from the above schema using mongoose
-var Article = mongoose.model("Article", ArticleSchema);
+const Article = mongoose.model("Article", ArticleSchema);
 
 //export the Article model
 module.exports = Article;
